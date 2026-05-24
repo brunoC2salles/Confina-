93e8044ab7198946bc2e3dd2ba6cd444e6b6e6c9
brunoC2salles <166764066+brunoC2salles@users.noreply.github.com>
2026-05-24 11:55:04 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_SUPABASE_URL
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY
+const url = (import.meta as any).env.VITE_SUPABASE_URL as string
+const key = (import.meta as any).env.VITE_SUPABASE_ANON_KEY as string
 
 if (!url || !key) throw new Error('Variáveis de ambiente Supabase não encontradas')
 
