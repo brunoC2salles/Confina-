fb8db7a28959a35f4b06d80e6e6a461cfc1c5e5f
brunoC2salles <166764066+brunoC2salles@users.noreply.github.com>
2026-07-04 11:12:57 -0300

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const url = (import.meta as any).env.VITE_SUPABASE_URL as string
-const key = (import.meta as any).env.VITE_SUPABASE_ANON_KEY as string
-
-if (!url || !key) throw new Error('Variáveis de ambiente Supabase não encontradas')
-
-export const supabase = createClient(url, key)
