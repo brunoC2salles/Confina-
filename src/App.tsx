cd9c765d8ec023015de1141f52cf095734a7413e
brunoC2salles <166764066+brunoC2salles@users.noreply.github.com>
2026-07-09 08:50:21 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { AuthProvider } from '@/contexts/AuthContext'
 import ProtectedRoute from '@/components/common/ProtectedRoute'
 import { Layout } from '@/components/common/Layout'
 import Login from '@/pages/Login'
+import EsqueciSenha from '@/pages/EsqueciSenha'
+import RedefinirSenha from '@/pages/RedefinirSenha'
 import Dashboard from '@/pages/Dashboard'
 import Lotes from '@/pages/Lotes'
 import Vendas from '@/pages/Vendas'
@@ -21,6 +23,8 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/esqueci-senha" element={<EsqueciSenha />} />
+          <Route path="/redefinir-senha" element={<RedefinirSenha />} />
           <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
             <Route index element={<Dashboard />} />
             <Route path="lotes"        element={<Lotes />} />
